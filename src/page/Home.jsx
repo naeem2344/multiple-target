096acb364607965c50f2e108169391c284edc011
Naeem Akhter <naeem@arnxt.com>
2025-10-10 11:49:49 +0530

    dskjfh

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { QRCodeSVG } from 'qrcode.react'
 import '../style.css';
 
 const Home = () => {
+  useEffect(() => {
+     return () => {
+      const sceneEl = document.querySelector("a-scene");
+      if (sceneEl && sceneEl.systems && sceneEl.systems["mindar-image"]) {
+        try {
+          sceneEl.systems["mindar-image"].stop();
+          sceneEl.systems["mindar-image"].renderer?.dispose?.();
+        } catch (err) {
+          console.warn("MindAR cleanup warning:", err);
+        }
+      }
+      sceneEl?.parentNode?.removeChild(sceneEl);
+    }
+  },[])
   return (
     <div className='target__image__container'>
       <div className='target__image__sub__container'>
