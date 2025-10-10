131e824f25cee1cd2ff37d7ad7dceb5981948602
Naeem Akhter <naeem@arnxt.com>
2025-10-10 18:23:39 +0530

    doston

--- src/page/ShowTarget.jsx
+++ src/page/ShowTarget.jsx
@@ -36,7 +36,7 @@ const ShowTarget = () => {
       clearTimeout(loginTime)
       clearTimeout(discountTime)
     }
-  }, [targetDetected , loginModal , couponModal , setCouponModal , setLoginModal , setTargetDetacted]);
+  }, [targetDetected , loginModal , couponModal , setCouponModal , setLoginModal , setTargetDetacted , modalKey ,discountKey]);
 
 
   return (
