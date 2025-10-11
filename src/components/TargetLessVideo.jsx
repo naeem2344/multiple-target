c4bab1f0c4941c3d5ac82f694c86cec89308d1ff
Naeem Akhter <naeem@arnxt.com>
2025-10-11 15:08:59 +0530

    css added

--- src/components/TargetLessVideo.jsx
+++ src/components/TargetLessVideo.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const TargetLessVideo = () => {
-
     return (
-        <div>
-            <video width="640" height="360" controls>
-                <source src="/assets/videos/atal-bihari-vajpayee.mp4" type="video/mp4" />
+        <div className="video__wrapper">
+            <video 
+                className="responsive__video" 
+                controls 
+                poster="/assets/thumbnails/atal-bihari-vajpayee.jpg"
+            >
+                <source src="/assets/videos/dr-manmohan-singh.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
         </div>
--- src/page/SingleVideo.jsx
+++ src/page/SingleVideo.jsx
@@ -1,9 +1,11 @@
 import React, { lazy, Suspense } from 'react'
-const TargetLessVideo = lazy(() => import('../components/TargetLessVideo'));
 import '../style.css'
+
+const TargetLessVideo = lazy(() => import('../components/TargetLessVideo'))
+
 const SingleVideo = () => {
     return (
-        <Suspense fallback={<><p>Please wait...</p></>}>
+        <Suspense fallback={<p className="loading__text">Please wait...</p>}>
             <div className='single__video__container'>
                 <TargetLessVideo />
             </div>
